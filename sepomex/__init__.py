43bac9811d74c272d461885e52f95c788fcf762f
PAEZ ORTEGA OSWALDO EMMANUEL <46234487+Osw1997@users.noreply.github.com>
2022-03-29 00:37:31 -0600

    Update __init__.py

--- sepomex/__init__.py
+++ sepomex/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from Scripts import carga_datos_sepomex
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = 'cura_deuda_test'
     app.config.from_object('config.Config')
+
+    # Se inicializan las tablas
+    carga_datos_sepomex.CargaDatos()
 
     # Se importan los archivos de rutas
     with app.app_context():
