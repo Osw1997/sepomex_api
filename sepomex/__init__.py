edebbc53011a669871d6fd2d8ba93ee87cc6b1c8
PAEZ ORTEGA OSWALDO EMMANUEL <46234487+Osw1997@users.noreply.github.com>
2022-03-30 00:14:26 -0600

    Se incluyo la herramienta SWAGGER para exponer la API mediante una UI

--- sepomex/__init__.py
+++ sepomex/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from Scripts import carga_datos_sepomex
 
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = 'cura_deuda_test'
     app.config.from_object('config.Config')
+
+
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': 'SEPOMEX_API'
+        }
+    )
+    app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
 
     # Se inicializan las tablas
     carga_datos_sepomex.CargaDatos()
